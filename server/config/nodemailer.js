9bd57d194a03a902d6b42b340450952d7a48031f
Ritu Agarwal <aritu9169@gmail.com>
2026-07-26 15:52:26 +0530

    fix: export default transporter instance in nodemailer config

---
+++ server/config/nodemailer.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer')
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
+  },
+  family: 4
+})
+
+module.exports = transporter
